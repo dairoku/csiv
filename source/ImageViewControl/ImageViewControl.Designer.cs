ea9e641d16b8c729566b5263bfb14b889fa9a9d2
Dairoku Sekiguchi <dairoku@gmail.com>
2022-04-07 23:00:14 +0900

    Added initial files

---
+++ source/ImageViewControl/ImageViewControl.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ImageViewControl
+{
+    partial class ImageViewControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mHorizScrollBar = new System.Windows.Forms.HScrollBar();
+            this.mVertScrollBar = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // mHorizScrollBar
+            // 
+            this.mHorizScrollBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mHorizScrollBar.Location = new System.Drawing.Point(0, 495);
+            this.mHorizScrollBar.Name = "mHorizScrollBar";
+            this.mHorizScrollBar.Size = new System.Drawing.Size(491, 17);
+            this.mHorizScrollBar.TabIndex = 0;
+            this.mHorizScrollBar.Visible = false;
+            this.mHorizScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.mScrollBars_Scroll);
+            // 
+            // mVertScrollBar
+            // 
+            this.mVertScrollBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mVertScrollBar.Location = new System.Drawing.Point(495, 0);
+            this.mVertScrollBar.Name = "mVertScrollBar";
+            this.mVertScrollBar.Size = new System.Drawing.Size(17, 495);
+            this.mVertScrollBar.TabIndex = 1;
+            this.mVertScrollBar.Visible = false;
+            this.mVertScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.mScrollBars_Scroll);
+            // 
+            // ImageViewControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mVertScrollBar);
+            this.Controls.Add(this.mHorizScrollBar);
+            this.Name = "ImageViewControl";
+            this.Size = new System.Drawing.Size(512, 512);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ImageViewControl_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ImageViewControl_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ImageViewControl_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ImageViewControl_MouseUp);
+            this.Resize += new System.EventHandler(this.ImageViewControl_Resize);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar mHorizScrollBar;
+        private System.Windows.Forms.VScrollBar mVertScrollBar;
+    }
+}
---
+++ source/SampleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// =============================================================================
+//  Program.cs
+//
+//  Written in 2022 by Dairoku Sekiguchi (sekiguchi at acm dot org)
+//
+//  To the extent possible under law, the author(s) have dedicated all copyright
+//  and related and neighboring rights to this software to the public domain worldwide.
+//  This software is distributed without any warranty.
+//
+//  You should have received a copy of the CC0 Public Domain Dedication along with
+//  this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+// =============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
